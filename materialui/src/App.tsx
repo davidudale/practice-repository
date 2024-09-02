19e40e550e68f1dcc294ae8e0ebd89d4cd768b44
davidudale <davison.any@gmail.com>
2024-09-02 02:39:42 +0100

    Changes effected

---
+++ materialui/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import MuiButtons  from "./component/MuiButtons"
+
+
+function App() {
+  return (
+    <div>
+     <MuiButtons/>
+    </div>
+  );
+}
+
+export default App;
---
+++ materialui/src/component/MuiButtons.tsx
@@ -0,0 +1,98 @@
+import React from "react";
+import { Stack, Button, IconButton } from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
+function muibuttons() {
+  return (
+    <Stack spacing={4}>
+      <Stack spacing={2} direction="row">
+        <Button variant="text">Text</Button>
+        <Button variant="contained">contained</Button>
+        <Button variant="outlined">outlined</Button>
+      </Stack>
+      <Stack spacing={2} direction="row">
+        <Button variant="contained" color="primary">
+          Primary
+        </Button>
+        <Button variant="contained" color="secondary">
+          Secondary
+        </Button>
+        <Button variant="contained" color="error">
+          Error
+        </Button>
+        <Button variant="contained" color="warning">
+          Warning
+        </Button>
+        <Button variant="contained" color="success">
+          Success
+        </Button>
+        <Button variant="contained" color="info">
+          Info
+        </Button>
+      </Stack>
+      <Stack spacing={2} direction="row">
+        <Button variant="outlined" color="primary">
+          Primary
+        </Button>
+        <Button variant="outlined" color="secondary">
+          Secondary
+        </Button>
+        <Button variant="outlined" color="error">
+          Error
+        </Button>
+        <Button variant="outlined" color="warning">
+          Warning
+        </Button>
+        <Button variant="outlined" color="success">
+          Success
+        </Button>
+        <Button variant="outlined" color="info">
+          Info
+        </Button>
+      </Stack>
+      <Stack spacing={2} direction="row">
+        <Button variant="text" color="primary">
+          Primary
+        </Button>
+        <Button variant="text" color="secondary">
+          Secondary
+        </Button>
+        <Button variant="text" color="error">
+          Error
+        </Button>
+        <Button variant="text" color="warning">
+          Warning
+        </Button>
+        <Button variant="text" color="success">
+          Success
+        </Button>
+        <Button variant="text" color="info">
+          Info
+        </Button>
+      </Stack>
+      <Stack spacing={2} display="block" direction="row">
+        <Button variant="contained" size="small">
+          small
+        </Button>
+        <Button variant="contained" size="medium">
+          medium
+        </Button>
+        <Button variant="contained" size="large">
+          large
+        </Button>
+      </Stack>
+      <Stack spacing={2} direction="row">
+        <Button variant="contained" startIcon={<SendIcon />}>
+          small
+        </Button>
+        <Button variant="contained" endIcon={<SendIcon />}>
+          small
+        </Button>
+        <IconButton aria-label="send" color="success" size="small">
+            <SendIcon />
+        </IconButton>
+      </Stack>
+    </Stack>
+  );
+}
+
+export default muibuttons;
